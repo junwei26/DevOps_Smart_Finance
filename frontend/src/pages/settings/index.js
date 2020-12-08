9bff7f5cd1f33d1e8bc1f4170262995388d3cae3
joelczk <joelchang97@gmail.com>
2020-12-08 20:16:37 +0800

    Used React Bootstrap for settings page

--- frontend/src/pages/settings/index.js
+++ frontend/src/pages/settings/index.js
@@ -1,27 +1,31 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "./index.scss";
 
 const Settings = () => {
   return (
     <div className="container">
       <div className="title">Settings</div>
-      <form>
+      <Form>
         <div>
-          <input type="text" name="username" placeholder="Enter your username" />
+          <Form.Control type="text" name="username" placeholder="Enter your username" />
         </div>
         <div>
-          <input type="text" name="username" placeholder="Enter your old password" />
+          <Form.Control type="text" name="oldPassword" placeholder="Enter your old password" />
         </div>
         <div>
-          <input type="text" name="username" placeholder="Enter your new password" />
+          <Form.Control type="text" name="newPassword" placeholder="Enter your new password" />
         </div>
         <div>
-          <input type="text" name="username" placeholder="Repeat your new password" />
+          <Form.Control type="text" name="repeatPassword" placeholder="Repeat your new password" />
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <Button variant="success" type="submit">
+            Submit
+          </Button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 };
