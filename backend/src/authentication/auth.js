5b438102e32b55ffb6f70ae458e345f890d9810c
junwei26 <limjunwei26@gmail.com>
2021-01-24 14:41:29 +0800

    added jwt authentication

---
+++ backend/src/authentication/auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const tokenSecret = "test";
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, tokenSecret, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
